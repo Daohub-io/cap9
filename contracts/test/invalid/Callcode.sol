c58177190fecee8a9eaeb71399f3e5723c4b570b
Jacob Payne <info@latrasis.com>
2018-02-27 00:11:37 +0300

    Fix Callcode test

--- contracts/test/invalid/Callcode.sol
+++ contracts/test/invalid/Callcode.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.17;
 contract Callcode {
     function foo(address a, uint gas) public returns (bool v) {
         assembly {
-            v := call(gas, a, 0, 0, 0, 0, 0)
+            v := callcode(gas, a, 0, 0, 0, 0, 0)
         }
     }
 }
