d7d3a4a076faead9aed5a746352b3c7630d0ef4b
Jake O'Shannessy <joshannessy@gmail.com>
2018-03-05 21:00:28 +0300

    Removed unnecessary logs.

--- contracts/test/valid/Adder.sol
+++ contracts/test/valid/Adder.sol
@@ -1,16 +1,6 @@
 pragma solidity ^0.4.17;
 
 contract Adder {
-    event LogFundsReceivedAdder(address sender, uint amount);
-    event LogFundsSent(address receiver, uint amount);
-
-    function Adder() payable {
-        LogFundsReceivedAdder(msg.sender, msg.value);
-    }
-    function() payable {
-        LogFundsReceivedAdder(msg.sender, msg.value);
-    }
-
     function add(uint a, uint b) pure public returns (uint256) {
         return a + b;
     }
--- contracts/test/valid/Multiply.sol
+++ contracts/test/valid/Multiply.sol
@@ -1,16 +1,6 @@
 pragma solidity ^0.4.17;
 
 contract Multiply {
-    event LogFundsReceivedMultiply(address sender, uint amount);
-    event LogFundsSent(address receiver, uint amount);
-
-    function Multiply() payable {
-        LogFundsReceivedMultiply(msg.sender, msg.value);
-    }
-    function() payable {
-        LogFundsReceivedMultiply(msg.sender, msg.value);
-    }
-
     function multiply(uint a, uint b) pure public returns (uint256) {
         return a * b;
     }
