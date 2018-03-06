ab372ca628277ddce2788632ca7df10dbcbe0551
Jacob Payne <info@latrasis.com>
2018-03-06 19:47:29 +0300

    Setup 1/4

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -12,12 +11,21 @@ export default {
 </script>
 
 <style>
+* {
+  box-sizing: border-box;  
+}
+
+html, body, #app {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
 </style>
