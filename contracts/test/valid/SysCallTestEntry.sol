d0cab7ae52990139411641d9bf0b2fcb438f5717
Jacob Payne <info@latrasis.com>
2019-02-20 15:34:35 -0800

    Add Tests + Check for Proc Id

---
+++ contracts/test/valid/SysCallTestEntry.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.17;
+
+import "../../BeakerContract.sol";
+
+contract SysCallTestEntry is BeakerContract {
+    // Log to no topics
+    function A() public returns (uint32) {
+        return set_entry(1, "TestWrite");
+    }
+}
