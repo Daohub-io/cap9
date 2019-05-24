6c3cfbd7b579e1b93e8aa0906af5abbd1160ecf6
Jake O'Shannessy <joshannessy@gmail.com>
2019-05-24 16:47:58 +1000

    Update windows build command

--- kernel-ewasm/build.bat
+++ kernel-ewasm/build.bat
@@ -1,3 +1,7 @@
+@echo OFF
+
+rustup target add wasm32-unknown-unknown
+cargo install pwasm-utils-cli --bin wasm-build --force
 
 cargo build --release --target wasm32-unknown-unknown
 wasm-build --target=wasm32-unknown-unknown .\target kernel-ewasm
