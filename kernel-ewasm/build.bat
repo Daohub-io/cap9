660db713f06d35e59da342a8585ec82ed44d8e5f
Jake O'Shannessy <joshannessy@gmail.com>
2019-05-28 16:06:01 +1000

    Fix std build

--- kernel-ewasm/build.bat
+++ kernel-ewasm/build.bat
@@ -1,7 +1,7 @@
 rustup target add wasm32-unknown-unknown
-cargo install pwasm-utils-cli --bin wasm-build --force --version 0.6.0
+cargo install pwasm-utils-cli --bin wasm-build --version 0.6.0
 
-cargo build --release --target wasm32-unknown-unknown
+cargo build --release --target wasm32-unknown-unknown --no-default-features
 wasm-build --target=wasm32-unknown-unknown .\target kernel-ewasm
 REM ..\..\wasm-utils\target\debug\wasm-build.exe --target=wasm32-unknown-unknown .\target kernel-ewasm
 
--- kernel-ewasm/build.sh
+++ kernel-ewasm/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 rustup target add wasm32-unknown-unknown
-cargo install pwasm-utils-cli --bin wasm-build --force --version 0.6.0
+cargo install pwasm-utils-cli --bin wasm-build --version 0.6.0
 
-cargo build --release --target wasm32-unknown-unknown
+cargo build --release --target wasm32-unknown-unknown --no-default-features
 wasm-build --target=wasm32-unknown-unknown ./target kernel-ewasm
 
 rm -dr ./build
