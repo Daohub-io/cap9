a85bbdb93840cffda874a6f7babd02361353cf6d
Jake O'Shannessy <joshannessy@gmail.com>
2019-06-05 15:42:34 +1000

    kernel: set number of memory pages to 3

--- kernel-ewasm/build.sh
+++ kernel-ewasm/build.sh
@@ -2,8 +2,10 @@
 
 rustup target add wasm32-unknown-unknown
 cargo install pwasm-utils-cli --bin wasm-build --version 0.6.0
+cargo install --path ../cap9-build --bin cap9-build --force
 
 cargo build --release --target wasm32-unknown-unknown --no-default-features
+cap9-build set-mem --pages 3 ./target/wasm32-unknown-unknown/release/kernel_ewasm.wasm ./target/wasm32-unknown-unknown/release/kernel_ewasm.wasm
 wasm-build --target=wasm32-unknown-unknown ./target kernel-ewasm
 
 rm -dr ./build
