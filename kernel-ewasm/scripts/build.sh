6d67a70b70dbc7645a45519833198f8b6a0c4813
Jake O'Shannessy <joshannessy@gmail.com>
2019-07-01 11:25:36 +1000

    Simplify and unify build scripts.

--- kernel-ewasm/scripts/build.sh
+++ kernel-ewasm/scripts/build.sh
@@ -1,48 +1,52 @@
 #!/bin/bash
-rustup target add wasm32-unknown-unknown
-cargo install pwasm-utils-cli --bin wasm-build --version 0.6.0
+set -e
+set -o pipefail
 
+# Make sure wasm32 is an available compile target for rust.
+rustup target add wasm32-unknown-unknown
+# Install wasm-build if it is not installed.
+if wasm-build --version; then
+    echo "wasm-build already installed"
+else
+    cargo install pwasm-utils-cli --bin wasm-build --version 0.6.0
+fi
+
+# Compile all of the examples.
 cargo build --examples --target wasm32-unknown-unknown --release --features std
 
+# Compile everything else.
 cargo build --release --target wasm32-unknown-unknown --no-default-features --features "panic_with_msg"
 pushd cap9-kernel
+# Recompile the kernel with panic messages.
 cargo build --release --target wasm32-unknown-unknown --no-default-features --features "panic_with_msg"
 popd
+# Increase the number of memory pages in the kernel.
 cargo run --package cap9-build -- set-mem --pages 4 ./target/wasm32-unknown-unknown/release/cap9_kernel.wasm ./target/wasm32-unknown-unknown/release/cap9_kernel.wasm
+# Pass the raw WASM output through the wasm-build post-processor.
 wasm-build --target=wasm32-unknown-unknown ./target cap9-kernel
 
 # Copy Examples
 cp ./target/wasm32-unknown-unknown/release/examples/*.wasm ./target/wasm32-unknown-unknown/release
 
-cargo run --package cap9-build -- build-proc ./target/wasm32-unknown-unknown/release/writer_test.wasm ./target/wasm32-unknown-unknown/release/writer_test.wasm
-cargo run --package cap9-build -- set-mem --pages 4 ./target/wasm32-unknown-unknown/release/writer_test.wasm ./target/wasm32-unknown-unknown/release/writer_test.wasm
-wasm-build --target=wasm32-unknown-unknown ./target writer_test
+# Pass example contracts through the procedure build process.
+function build_procedure {
+    echo "Building $1"
+    cargo run --package cap9-build -- build-proc ./target/wasm32-unknown-unknown/release/$1.wasm ./target/wasm32-unknown-unknown/release/$1.wasm
+    cargo run --package cap9-build -- set-mem --pages 4 ./target/wasm32-unknown-unknown/release/$1.wasm ./target/wasm32-unknown-unknown/release/$1.wasm
+    wasm-build --target=wasm32-unknown-unknown ./target $1
+}
 
-cargo run --package cap9-build -- build-proc ./target/wasm32-unknown-unknown/release/entry_test.wasm ./target/wasm32-unknown-unknown/release/entry_test.wasm
-wasm-build --target=wasm32-unknown-unknown ./target entry_test
+build_procedure writer_test
+build_procedure entry_test
+build_procedure caller_test
+build_procedure logger_test
+build_procedure register_test
+build_procedure logger_test
+build_procedure register_test
+build_procedure delete_test
+build_procedure account_call_test
+build_procedure acl
 
-cargo run --package cap9-build -- build-proc ./target/wasm32-unknown-unknown/release/caller_test.wasm ./target/wasm32-unknown-unknown/release/caller_test.wasm
-cargo run --package cap9-build -- set-mem --pages 4 ./target/wasm32-unknown-unknown/release/caller_test.wasm ./target/wasm32-unknown-unknown/release/caller_test.wasm
-wasm-build --target=wasm32-unknown-unknown ./target caller_test
-
-cargo run --package cap9-build -- build-proc ./target/wasm32-unknown-unknown/release/logger_test.wasm ./target/wasm32-unknown-unknown/release/logger_test.wasm
-cargo run --package cap9-build -- set-mem --pages 4 ./target/wasm32-unknown-unknown/release/logger_test.wasm ./target/wasm32-unknown-unknown/release/logger_test.wasm
-wasm-build --target=wasm32-unknown-unknown ./target logger_test
-
-cargo run --package cap9-build -- build-proc ./target/wasm32-unknown-unknown/release/register_test.wasm ./target/wasm32-unknown-unknown/release/register_test.wasm
-cargo run --package cap9-build -- set-mem --pages 4 ./target/wasm32-unknown-unknown/release/register_test.wasm ./target/wasm32-unknown-unknown/release/register_test.wasm
-wasm-build --target=wasm32-unknown-unknown ./target register_test
-
-cargo run --package cap9-build -- build-proc ./target/wasm32-unknown-unknown/release/delete_test.wasm ./target/wasm32-unknown-unknown/release/delete_test.wasm
-cargo run --package cap9-build -- set-mem --pages 4 ./target/wasm32-unknown-unknown/release/delete_test.wasm ./target/wasm32-unknown-unknown/release/delete_test.wasm
-wasm-build --target=wasm32-unknown-unknown ./target delete_test
-
-cargo run --package cap9-build -- build-proc ./target/wasm32-unknown-unknown/release/account_call_test.wasm ./target/wasm32-unknown-unknown/release/account_call_test.wasm
-cargo run --package cap9-build -- set-mem --pages 4 ./target/wasm32-unknown-unknown/release/account_call_test.wasm ./target/wasm32-unknown-unknown/release/account_call_test.wasm
-wasm-build --target=wasm32-unknown-unknown ./target account_call_test
-
-cargo run --package cap9-build -- build-proc ./target/wasm32-unknown-unknown/release/acl.wasm ./target/wasm32-unknown-unknown/release/acl.wasm
-cargo run --package cap9-build -- set-mem --pages 4 ./target/wasm32-unknown-unknown/release/acl.wasm ./target/wasm32-unknown-unknown/release/acl.wasm
-wasm-build --target=wasm32-unknown-unknown ./target acl
-
+# external_contract is just a regular contract and does not need to go through
+# the cap9 procedure build process.
 wasm-build --target=wasm32-unknown-unknown ./target external_contract
--- kernel-ewasm/scripts/test.sh
+++ kernel-ewasm/scripts/test.sh
@@ -1,1 +1,7 @@
-cargo test --lib
+#!/bin/bash
+set -e
+set -o pipefail
+
+scripts/build.sh
+cargo test
+./node_modules/.bin/mocha -r ts-node/register --recursive tests/**.ts tests/**/**.ts
