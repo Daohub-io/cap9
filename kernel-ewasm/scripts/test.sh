f30012f45cfd680b8809aab9274819e67f9789a7
Jake O'Shannessy <joshannessy@gmail.com>
2019-07-03 13:56:34 +1000

    Add parallel tests (only used optionally).

--- kernel-ewasm/scripts/test.sh
+++ kernel-ewasm/scripts/test.sh
@@ -2,6 +2,15 @@
 set -e
 set -o pipefail
 
+# Use the flag -p to run parallel tests using the "mocha-parallel-tests"
+# package.
+
+MOCHA_CMD=mocha
+
+if [ "$1" = "-p" ]; then
+    MOCHA_CMD=mocha-parallel-tests
+fi
+
 scripts/build.sh
 cargo test
-./node_modules/.bin/mocha -r ts-node/register --recursive tests/**.ts tests/**/**.ts
+./node_modules/.bin/${MOCHA_CMD} -r ts-node/register --recursive tests/**.ts tests/**/**.ts
